a18a6892b79f9603369a5e3ef8987e166ba7fa6a
Argentum11 <92793837+Argentum11@users.noreply.github.com>
2024-08-27 13:12:48 +0800

    feat: save user email to localStorage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +16,37 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    /* We use afterNextRender for two reasons:
+     1. In the template-driven approach, we need to wait for the template to render before the form is fully initialized.
+     2. afterRender and afterNextRender are meant for operations that should only be executed on the client, not on the server.
+        This makes them preferable to ngAfterViewInit in this context. */
+    afterNextRender(() => {
+      const formSubscription = this.form()
+        .valueChanges?.pipe(
+          /* The debounceTime operator delays the emission of values from the source Observable,
+           discarding any values that arrive within the specified time frame.
+           This prevents the subsequent function from being triggered while the user is still typing.
+           Additionally, it improves performance by reducing the number of unnecessary operations during rapid user input. */
+          debounceTime(500),
+        )
+        .subscribe({
+          next: (value) =>
+            window.localStorage.setItem(
+              'saved-login-form',
+              JSON.stringify({
+                email: value.email,
+              }),
+            ),
+        });
+
+      this.destroyRef.onDestroy(() => formSubscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData: NgForm) {
     if (formData.invalid) {
       return;
